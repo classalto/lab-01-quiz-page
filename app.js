659a36d6ab4bb602c34c4ac037b3c6796f20c104
VICTOR WILLIAMS <vbw293@gmail.com>
2021-01-11 15:56:11 -0800

    add event listener in app.js and validate user prompt and comfirm functionality

--- app.js
+++ app.js
@@ -2,8 +2,16 @@
 const quizButton = document.getElementById('quiz-button');
 const resultsDiv = document.getElementById('secret-div');
 
-console.log(quizButton, resultsDiv);
-
 // initialize state
 
+
 // set event listeners to update state and DOM
+quizButton.addEventListener('click', () => {
+    
+    const userName = prompt('What is your first name?');
+    
+    const userAgree = confirm(`${userName}, are you sure you would like to take this quiz?`);
+
+    if(!userAgree) return;
+
+});
