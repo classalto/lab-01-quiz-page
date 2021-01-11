f64271e7fa7211b84b4a527c263507acffe96929
VICTOR WILLIAMS <vbw293@gmail.com>
2021-01-11 15:27:50 -0800

    add dom elements to app.js with validation

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 // import functions and grab DOM elements
-const 
+const quizButton = document.getElementById('quiz-button');
+const resultsDiv = document.getElementById('secret-div');
+
+console.log(quizButton, resultsDiv);
+
 // initialize state
 
 // set event listeners to update state and DOM
