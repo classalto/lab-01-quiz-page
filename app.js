8e660d4bd2a0878c1f86e7a781ca3ad0bb807772
VICTOR WILLIAMS <vbw293@gmail.com>
2021-01-11 17:24:35 -0800

    create prompt checking function in utils.js

--- app.js
+++ app.js
@@ -1,17 +1,40 @@
 // import functions and grab DOM elements
+import { countsAsAYes } from './utils.js';
+
 const quizButton = document.getElementById('quiz-button');
 const resultsDiv = document.getElementById('secret-div');
 
 // initialize state
-
+let answersCorrect = 0;
 
 // set event listeners to update state and DOM
 quizButton.addEventListener('click', () => {
     
     const userName = prompt('What is your first name?');
+    if (!userName) return;
     
     const userAgree = confirm(`${userName}, are you sure you would like to take this quiz?`);
 
-    if(!userAgree) return;
+    if (!userAgree) return;
+
+    // User prompt validation
+    const userAnswer1 = prompt(`${userName}, did Liam O'brien appear as a voice actor in the cartoon Star Wars Rebels? (yes or no)`);
+    if (countsAsAYes(userAnswer1)) answersCorrect++;
+    
+    const userAnswer2 = prompt(`${userName}, was one of Liam O'Brien's major anime roles as Gaara in Naruto? (yes or no)`);
+    if (countsAsAYes(userAnswer1)) answersCorrect++;
+    
+    const userAnswer3 = prompt(`${userName} did Liam O'brien appear as a voice actor in the cartoon Star Wars Rebels? (yes or no)`);
+    if (countsAsAYes(userAnswer1)) answersCorrect++;
+    
+    
+
+
+
+
+    
+    
+
+
 
 });
--- test/example.test.js
+++ test/example.test.js
@@ -1,8 +1,21 @@
 // IMPORT MODULES under test here:
-// import { example } from '../example.js';
+import { countsAsAYes } from '../utils.js';
 
 const test = QUnit.test;
 
+test('should return false when given hell yes as an input', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = false;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = countsAsAYes('hell yes');
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
 test('time to test a function', (expect) => {
     //Arrange
     // Set up your arguments and expectations
